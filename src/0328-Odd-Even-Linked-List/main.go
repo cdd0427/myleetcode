e3207bd417576be3a20dda9c6feff52fd4016a3e
cdd <20517610@qq.com>
2020-11-13 23:28:56 +0800

    201113 m1

---
+++ src/0328-Odd-Even-Linked-List/main.go
@@ -0,0 +1,25 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil{
+		return head
+	}
+	if head.Next==nil{
+		return head
+	}
+	odd,even,evenHead:=head,head.Next,head.Next
+	for even!=nil&&even.Next!=nil{
+		odd.Next=even.Next
+		odd=odd.Next
+		even.Next=odd.Next
+		even=even.Next
+	}
+	odd.Next = evenHead
+	return head
+}
+
